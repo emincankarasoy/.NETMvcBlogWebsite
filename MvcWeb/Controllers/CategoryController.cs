0658a5a99a13a2ef69734b0899ae737fa8235b19
Emincan Karasoy <emincannkarasoy@gmail.com>
2022-03-19 00:33:32 +0300

    add category manager and controller

---
+++ MvcWeb/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private CategoryManager categoryManager = new CategoryManager();
+        public ActionResult Index()
+        {
+            var categoryvalues = categoryManager.GetAll();
+            return View(categoryvalues);
+        }
+    }
+}
