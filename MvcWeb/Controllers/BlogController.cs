f5eba84d75f56ee392a1829637bec0b3849d1868
Emincan Karasoy <emincannkarasoy@gmail.com>
2022-03-19 21:48:07 +0300

    complete main page featured post partition

--- MvcWeb/Controllers/BlogController.cs
+++ MvcWeb/Controllers/BlogController.cs
@@ -17,6 +17,33 @@ namespace MvcWeb.Controllers
         [ChildActionOnly]
         public PartialViewResult FeaturedBlogs()
         {
+            var postFirst = blogManager.getAll().OrderByDescending(x => x.BlogID).Where(x => x.CategoryID == 2);
+            var postSecond = blogManager.getAll().OrderByDescending(x => x.BlogID).Where(x => x.CategoryID == 3);
+            var postThird = blogManager.getAll().OrderByDescending(x => x.BlogID).Where(x => x.CategoryID == 4);
+            var postFourth = blogManager.getAll().OrderByDescending(x => x.BlogID).Where(x => x.CategoryID == 1);
+            var postFifth = blogManager.getAll().OrderByDescending(x => x.BlogID).Where(x => x.CategoryID == 5);
+
+            ViewBag.postTitleFirst = postFirst.Select(x => x.BlogTitle).FirstOrDefault();
+            ViewBag.postImageFirst = postFirst.Select(x => x.BlogImage).FirstOrDefault();
+            ViewBag.postDateFirst  = postFirst.Select(x => x.BlogDate).FirstOrDefault();
+
+            ViewBag.postTitleSecond = postSecond.Select(x => x.BlogTitle).FirstOrDefault();
+            ViewBag.postImageSecond = postSecond.Select(x => x.BlogImage).FirstOrDefault();
+            ViewBag.postDateSecond  = postSecond.Select(x => x.BlogDate).FirstOrDefault();
+
+            ViewBag.postTitleThird = postThird.Select(x => x.BlogTitle).FirstOrDefault();
+            ViewBag.postImageThird = postThird.Select(x => x.BlogImage).FirstOrDefault();
+            ViewBag.postDateThird  = postThird.Select(x => x.BlogDate).FirstOrDefault();
+
+            ViewBag.postTitleFourth = postFourth.Select(x => x.BlogTitle).FirstOrDefault();
+            ViewBag.postImageFourth = postFourth.Select(x => x.BlogImage).FirstOrDefault();
+            ViewBag.postDateFourth = postFourth.Select(x => x.BlogDate).FirstOrDefault();
+
+            ViewBag.postTitleFifth = postFifth.Select(x => x.BlogTitle).FirstOrDefault();
+            ViewBag.postImageFifth = postFifth.Select(x => x.BlogImage).FirstOrDefault();
+            ViewBag.postDateFifth = postFifth.Select(x => x.BlogDate).FirstOrDefault();
+            ViewBag.postCategoryFifth = postFifth.Select(x => x.Category.CategoryName).FirstOrDefault();
+
             return PartialView();
         }
         [ChildActionOnly]
