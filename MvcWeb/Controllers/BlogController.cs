d28f1b252bc6e149c3067dd68c08f7f085867334
Emincan Karasoy <emincannkarasoy@gmail.com>
2022-03-19 11:41:47 +0300

    configure theme to partial view

---
+++ MvcWeb/Controllers/BlogController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcWeb.Controllers
+{
+    public class BlogController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [ChildActionOnly]
+        public PartialViewResult FeaturedBlogs()
+        {
+            return PartialView();
+        }
+        [ChildActionOnly]
+        public PartialViewResult BlogList()
+        {
+            return PartialView();
+        }
+        [ChildActionOnly]
+        public PartialViewResult NewBlogs() 
+        {
+            return PartialView();
+        }
+        [ChildActionOnly]
+        public PartialViewResult MailSubscribe() 
+        {
+            return PartialView();
+        }
+        [ChildActionOnly]
+        public PartialViewResult Footer()
+        {
+            return PartialView();
+        }
+    }
+}
